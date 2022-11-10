3bff793f966c6bfa95f4adb41aa5b0d0e9d91b69
nolanfoy <100647056+nolanfoy@users.noreply.github.com>
2022-11-10 15:35:38 -0600

    sort by fuel and location working

--- server.js
+++ server.js
@@ -24,15 +24,19 @@ let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
     }
 });
 
+
+
+
 // Serve static files from 'public' directory
 app.use(express.static(public_dir));
 
 
 // GET request handler for home page '/' (redirect to desired route)
-/*app.get('/', (req, res) => {
-    let home = ''; // <-- change this
+app.get('/', (req, res) => {
+    let home = '/country/USA'; // <-- change this
     res.redirect(home);
-});*/
+   
+})
 
 
 // Example GET request handler for data about a specific year
@@ -46,8 +50,112 @@ app.get('/country/:cid', (req, res) => {
                     plants.generation_gwh_2015, fuels.fuel, plants.url \
                     FROM plants INNER JOIN countries ON \
                     plants.countryId = countries.countryid INNER JOIN fuels ON plants.fuelId = fuels.fuelId WHERE countries.countryid = ?';
+        let response = template.toString();
+        let fillQuery = 'SELECT countryid FROM countries';
+        let fillData = '';
 
         db.all(query, cid, (err, rows) => {
+            let plant_data = '';
+            for (let i=0; i < rows.length; i++) {
+                plant_data = plant_data + '<tr>';
+                plant_data = plant_data + '<td>' + rows[i].name + '</td>';
+                plant_data = plant_data + '<td>' + rows[i].country + '</td>';
+                plant_data = plant_data + '<td>' + rows[i].capacity_mw + '</td>';
+                plant_data = plant_data + '<td>' + rows[i].generation_gwh_2015 + '</td>';
+                plant_data = plant_data + '<td>' + rows[i].fuel + '</td>';
+                plant_data = plant_data + '<td>' + rows[i].url + '</td>';
+                plant_data = plant_data + '</tr>';
+
+            }
+            response = response.replace('%%PLANT_INFO%%', plant_data);
+            db.all(fillQuery, (err, rows) => {
+                for (let i=0; i < rows.length; i++) {
+                    fillData = fillData + '<a href="/country/'+rows[i].countryid+'">'+rows[i].countryid+'</a>';
+                    console.log(rows);
+                }
+                response = response.replace('%%COUNTRY_OPTIONS%%', fillData);
+                res.status(200).type('html').send(response);
+                
+            });
+            
+            
+
+
+        });
+
+        
+        
+        
+    });
+});
+
+
+app.get('/fuel/:fid', (req, res) => {
+    let fid = req.params.fid;
+    console.log(req.params.selected_year);
+    fs.readFile(path.join(template_dir, 'index.html'), (err, template) => {
+        // modify `template` and send response
+        // this will require a query to the SQL database
+        let query = 'SELECT plants.name, countries.country, plants.capacity_mw, \
+                    plants.generation_gwh_2015, fuels.fuel, plants.url \
+                    FROM plants INNER JOIN countries ON \
+                    plants.countryId = countries.countryid INNER JOIN fuels ON plants.fuelId = fuels.fuelId WHERE fuels.fuel = ?';
+        let response = template.toString();
+        let fillQuery = 'SELECT countryid FROM countries';
+        let fillData = '';
+
+        db.all(query, fid, (err, rows) => {
+            // console.log(err);
+            // response = response.replace('%%MANUFACTURER%%', rows[0].mfr);
+            // response = response.replace('%%MFR_ALT_TEXT%%', 'logo for ' + rows[0].mfr);
+            // response = response.replace('%%MFR_IMAGE%%', '/images/' + mfr + '_logo.png');
+            let plant_data = '';
+            for (let i=0; i < rows.length; i++) {
+                plant_data = plant_data + '<tr>';
+                plant_data = plant_data + '<td>' + rows[i].name + '</td>';
+                plant_data = plant_data + '<td>' + rows[i].country + '</td>';
+                if (rows[i].generation_gwh_2015 == '') {
+                    plant_data = plant_data + '<td>NA</td>';
+                } else {
+                    plant_data = plant_data + '<td>' + rows[i].generation_gwh_2015 + '</td>';
+                }
+                
+                plant_data = plant_data + '<td>' + rows[i].capacity_mw + '</td>';
+                plant_data = plant_data + '<td>' + rows[i].fuel + '</td>';
+                plant_data = plant_data + '<td>' + rows[i].url + '</td>';
+                plant_data = plant_data + '</tr>';
+
+            }
+            response = response.replace('%%PLANT_INFO%%', plant_data);
+
+            db.all(fillQuery, (err, rows) => {
+                for (let i=0; i < rows.length; i++) {
+                    fillData = fillData + '<a href="/country/'+rows[i].countryid+'">'+rows[i].countryid+'</a>';
+                    console.log(rows);
+                }
+                response = response.replace('%%COUNTRY_OPTIONS%%', fillData);
+                res.status(200).type('html').send(response);
+                
+            });
+                
+            
+        });
+    });
+});
+
+
+app.get('/capacity/:cap', (req, res) => {
+    let cap = req.params.cap;
+    console.log(req.params.selected_year);
+    fs.readFile(path.join(template_dir, 'index.html'), (err, template) => {
+        // modify `template` and send response
+        // this will require a query to the SQL database
+        let query = 'SELECT plants.name, countries.country, plants.capacity_mw, \
+                    plants.generation_gwh_2015, fuels.fuel, plants.url \
+                    FROM plants INNER JOIN countries ON \
+                    plants.countryId = countries.countryid INNER JOIN fuels ON plants.fuelId = fuels.fuelId WHERE plants.capacity_mw >= ? AND plants.capacity_mw != ""';
+
+        db.all(query, cap, (err, rows) => {
             // console.log(err);
             console.log(rows);
             let response = template.toString();
@@ -60,7 +168,11 @@ app.get('/country/:cid', (req, res) => {
                 plant_data = plant_data + '<td>' + rows[i].name + '</td>';
                 plant_data = plant_data + '<td>' + rows[i].country + '</td>';
                 plant_data = plant_data + '<td>' + rows[i].capacity_mw + '</td>';
-                plant_data = plant_data + '<td>' + rows[i].generation_gwh_2015 + '</td>';
+                if (rows[i].generation_gwh_2015 == '') {
+                    plant_data = plant_data + '<td>NA</td>';
+                } else {
+                    plant_data = plant_data + '<td>' + rows[i].generation_gwh_2015 + '</td>';
+                }
                 plant_data = plant_data + '<td>' + rows[i].fuel + '</td>';
                 plant_data = plant_data + '<td>' + rows[i].url + '</td>';
                 plant_data = plant_data + '</tr>';
